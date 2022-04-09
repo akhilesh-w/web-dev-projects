06e0ddc9931d6d6af9bcd6d950ca9869136f8703
Akhilesh <akhi23waghmare@gmail.com>
2022-04-09 17:15:09 +0530

    first-dice-completed

--- diceRollChallenge/index.js
+++ diceRollChallenge/index.js
@@ -1,3 +1,9 @@
-var randomNumber1 = Math.random();
-var randomNumber1 = Math.floor((randomNumber1 * 6) + 1);
-console.log(randomNumber1);
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+
+var randomImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
