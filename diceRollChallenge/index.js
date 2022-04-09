a48b4581ed27fad6b4a104b92a5302da8ae8fe02
Akhilesh <akhi23waghmare@gmail.com>
2022-04-09 17:39:04 +0530

    finished-the-challenge

--- diceRollChallenge/index.js
+++ diceRollChallenge/index.js
@@ -19,3 +19,12 @@ var randomImage2 = "images/dice" + randomNumber2 + ".png";
 var image2 = document.querySelectorAll("img")[1];
 
 image2.setAttribute("src", randomImage2);
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = ("Player 1 Wins");
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = ("Player 2 Wins");
+} else {
+    document.querySelector("h1").innerHTML = ("Draw!!")
+}
