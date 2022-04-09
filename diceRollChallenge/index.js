5646a16ad316f736dad8fbde6e62b8cc164e743f
Akhilesh <akhi23waghmare@gmail.com>
2022-04-09 17:22:10 +0530

    added-second-dice

--- diceRollChallenge/index.js
+++ diceRollChallenge/index.js
@@ -7,3 +7,15 @@ var randomImageSource = "images/" + randomImage;
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
+
+//second dice 
+
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
+var randomImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource2 = "images/" + randomImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource2);
